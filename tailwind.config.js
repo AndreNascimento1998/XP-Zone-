144456cca8ad90d5e46a7e370061430510286ae4
Andre <andre.ncardoso@hotmail.com>
2024-08-12 13:54:43 -0300

    fix: lint and type

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,9 @@ export default {
         'btn-primary': 'linear-gradient(90deg, #E54B65 3.13%, #FF9A51 133.21%)',
         'btn-primary-hover': 'linear-gradient(90deg, #FF9A51 3.13%, #E54B65 133.21%)'
       },
+      backgroundColor: {
+        'primary': '#210948',
+      }
     },
   },
   plugins: [],
