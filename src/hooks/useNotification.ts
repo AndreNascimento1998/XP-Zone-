81607bafad25e92d2dbc75c50eaf312894e58bd2
Andre <andre.ncardoso@hotmail.com>
2024-08-12 17:18:49 -0300

    feat: new hooks

---
+++ src/hooks/useNotification.ts
@@ -0,0 +1,27 @@
+import { toast, ToastOptions } from 'react-toastify';
+
+const useNotification = () => {
+    const showSuccess = (message: string, options?: ToastOptions) => {
+        toast.success(message, {
+            bodyClassName: 'text-[white]',
+            bodyStyle: { background: '#19be3d' },
+            progressStyle: { background: '#fff0f0' },
+            style: { background: '#19be3d' },
+            ...options,
+        });
+    };
+
+    const showError = (message: string, options?: ToastOptions) => {
+        toast.error(message, {
+            bodyClassName: 'text-[white]',
+            bodyStyle: { background: '#e74c3c' },
+            progressStyle: { background: '#fff0f0' },
+            style: { background: '#e74c3c' },
+            ...options,
+        });
+    };
+
+    return { showSuccess, showError };
+};
+
+export default useNotification;
---
+++ src/pages/Login/ConfirmRegister.tsx
@@ -0,0 +1,39 @@
+import {Link} from "react-router-dom"
+import Button from "@/components/Button/Index.tsx"
+import {ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import {useEffect} from "react";
+import useNotification from "@/hooks/useNotification.ts";
+
+interface ConfirmRegisterProps {
+    name: string
+    email: string
+}
+
+const ConfirmRegister = ({ name, email }: ConfirmRegisterProps) => {
+    const { showSuccess } = useNotification()
+
+    useEffect(() => {
+       showSuccess('Usuário cadastrado com sucesso!')
+    }, [showSuccess])
+
+    return (
+        <>
+            <div className={'text-[white] text-[18px]'}>
+                <span>
+                    O usuário <span className={'font-bold text-[20px]'}> { name } </span>
+                    com e-mail <span className={'font-bold text-[20px]'}> { email } </span>
+                    foi cadastrado com sucesso.
+                </span>
+                <div className={'font-bold py-4'}>
+                    IMPORTANTE: O cadastro é meramente
+                    ilustrativo, utilize as contas "cadastradas" para interagir com as funcionalidades entre elas.
+                </div>
+                <Link to={'/login'}><Button >Retornar ao login</Button> </Link>
+            </div>
+            <ToastContainer />
+        </>
+    )
+}
+
+export default ConfirmRegister
