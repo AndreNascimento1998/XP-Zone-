253748dd18b82c2278962ebbd99e1e957c66292a
Andre <andre.ncardoso@hotmail.com>
2024-08-14 12:53:12 -0300

    feat: create hook notification

--- src/hooks/useNotification.ts
+++ src/hooks/useNotification.ts
@@ -1,27 +1,61 @@
 import { toast, ToastOptions } from 'react-toastify'
+import {useEffect, useMemo} from "react"
 
-const useNotification = () => {
-    const showSuccess = (message: string, options?: ToastOptions) => {
+interface IToastOptions {
+    message: string,
+    type: NotificationType,
+    optionsStyle?: ToastOptions
+}
+
+type NotificationType = 'messageSuccess' | 'messageError' | 'messageNeutral'
+
+const useNotification = ({message, type, optionsStyle}: IToastOptions) => {
+    const showMessageSuccess = (message: string, options?: ToastOptions) => {
         toast.success(message, {
             bodyClassName: 'text-[white]',
             bodyStyle: { background: '#19be3d' },
             progressStyle: { background: '#fff0f0' },
             style: { background: '#19be3d' },
             ...options,
+            ...optionsStyle,
         })
     }
 
-    const showError = (message: string, options?: ToastOptions) => {
+    const showMessageNeutral = (message: string, options?: ToastOptions) => {
+        toast(message, {
+            bodyClassName: 'text-[white]',
+            bodyStyle: { background: '#3498db' },
+            progressStyle: { background: '#fff0f0' },
+            style: { background: '#3498db' },
+            ...optionsStyle,
+            ...options,
+        })
+    }
+
+    const showMessageError = (message: string, options?: ToastOptions) => {
         toast.error(message, {
             bodyClassName: 'text-[white]',
             bodyStyle: { background: '#e74c3c' },
             progressStyle: { background: '#fff0f0' },
             style: { background: '#e74c3c' },
+            ...optionsStyle,
             ...options,
         })
     }
 
-    return { showSuccess, showError }
+    const isErrorMessage = useMemo(() => {
+        const typeMessage = {
+            messageError: showMessageError,
+            messageSuccess: showMessageSuccess,
+            messageNeutral: showMessageNeutral
+        }
+
+        return typeMessage[type]
+    }, [type])
+
+    useEffect(() => {
+        isErrorMessage(message)
+    }, [isErrorMessage, message])
 }
 
-export default useNotification;
+export default useNotification
