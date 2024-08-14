bcd2fe6929b6339f8b3566ed10c0b53d0dbdd6b7
Andre <andre.ncardoso@hotmail.com>
2024-08-14 16:24:32 -0300

    feat: new page

---
+++ src/views/LandingPage/Index.tsx
@@ -0,0 +1,16 @@
+import HeroSection from "@/pages/LandingPage/HeroSection.tsx";
+
+const LadingPage = () => {
+    return (
+        <div className={'bg-secondary text-white-light'}>
+            <div className={'flex flex-col gap-10'}>
+                <HeroSection />
+                <div>
+                    oii
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default LadingPage;
