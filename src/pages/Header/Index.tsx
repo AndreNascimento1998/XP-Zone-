a564e230095107dc74e1155dbf4e935134b4105e
Andre <andre.ncardoso@hotmail.com>
2024-08-19 17:23:51 -0300

    style: animation bell notification

--- src/pages/Header/Index.tsx
+++ src/pages/Header/Index.tsx
@@ -7,6 +7,7 @@ import MyDataIcon from "@/assets/Icons/Header/MyDataIcon.tsx"
 import LibraryIcon from "@/assets/Icons/Header/LibraryIcon.tsx"
 import FavoriteIcon from "@/assets/Icons/Header/FavoriteIcon.tsx"
 import ExitIcon from "@/assets/Icons/Header/ExitIcon.tsx"
+import '@/pages/Header/styles/Header.scss'
 const Header = () => {
     const itemsNotification = [
         {name: "um", callback: () => console.log('um')},
@@ -30,7 +31,7 @@ const Header = () => {
                 <div>
                     <ButtonCustom onItemClick={(item) => console.log(item)} items={itemsNotification}>
                         {itemsNotification && itemsNotification.length ? (
-                            <div className={'bg-card-light hover:bg-gray-500 hover:bg-opacity-40 rounded-full p-3'}><BellNotificationIcon/></div>) :
+                            <div className={'bg-card-light bell-icon hover:bg-gray-500 hover:bg-opacity-40 rounded-full p-3'}><BellNotificationIcon/></div>) :
                             (<div className={'bg-card-light hover:bg-gray-500 hover:bg-opacity-40 rounded-full p-3'}><BellIcon/></div>)
                         }
                     </ButtonCustom>
